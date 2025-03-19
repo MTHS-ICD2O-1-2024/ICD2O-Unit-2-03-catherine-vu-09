a3c4d233a21994007a060f8d7a0d924aae2a588b
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-03-19 17:19:10 +0000

    added css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+// Copyright (c) 2020 Catherine Vu All rights reserved
+//
+// Created by: Catherine Vu
+// Created on: March 19 2025
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function enterClicked() {
+const streetNumber = document.getElementById('streetNumber').value;
+const streetName = document.getElementById('streetName').value;
+}
+if (streetNumber && streetName) {
+const output = `Your address is: ${streetNumber} ${streetName}.`;
+document.getElementById('output').textContent = output;
+} 
 
